e956aac80c735005300522fa01f759f5ed882ab5
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-02-22 02:46:54 -0500

    Javascript and React.js A3

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,11 +1,27 @@
 import JavaScript from "./JavaScript";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import PathParameters from "./routing/PathParameters";
+import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
+import Add from "./Add";
+import TodoList from "./todos/TodoList";
 function Assignment3() {
  return (
-   <div className="container">
+   <div>
      <h1>Assignment 3</h1>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
+     <PathParameters/>
      <JavaScript/>
+     <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <Add a={3} b={4} />
+     <TodoList/>
    </div>
- );
+ )
 }
-
-  export default Assignment3;
+export default Assignment3;
