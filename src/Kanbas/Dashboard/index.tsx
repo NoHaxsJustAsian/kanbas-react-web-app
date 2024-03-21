03cd310657d92e9680df295913da190a5cd62501
Win Tongtawee <WinTongtawee@gmail.com>
2024-03-20 22:50:02 -0400

    indexcvuopewqte

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import { courses } from "../Database";
+import db from "../Database";
 function Dashboard() {
+  const [courses, setCourses] = useState(db.courses);
+  const course = {
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg"
+  };
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+                        _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+  const [course, setCourse] = useState({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg"
+  });
+
   return (
     <div className="p-4">
       <h1>Dashboard</h1>              <hr />
+      <h5>Course</h5>
+      <input value={course.name} className="form-control" />
+      <input value={course.number} className="form-control" />
+      <input value={course.startDate} className="form-control" type="date" />
+      <input value={course.endDate} className="form-control" type="date" />
+      <button onClick={addNewCourse} >
+        Add
+      </button>
       <h2>Published Courses (12)</h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
