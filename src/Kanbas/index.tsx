d1041b62eb66fc8a7668f938c8bd06ae9c732690
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-04-17 22:33:57 -0400

    a6 init

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -54,7 +54,7 @@ function Kanbas() {
     <div className="d-flex">
       <KanbasNavigation />
       <div>
-        <Routes>
+        <Routes> 
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
           <Route path="Dashboard" element={
